0ac32010c87083ad85a540054f8bfdb79b2071a7
Ashutoshbind15 <Ashutoshbind15@users.noreply.github.com>
2024-06-15 15:26:29 +0530

    initialize the course routes and also improve the backend project struct

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,20 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import connectDB from "./lib/db.js";
+import courseRoutes from "./routes/course.js";
 
 dotenv.config();
 const app = express();
 app.use(cors());
+app.use(express.json());
+connectDB();
 
 app.get("/", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
+
+app.use("/api/courses", courseRoutes);
 
 const PORT = process.env.PORT || 3000;
 
