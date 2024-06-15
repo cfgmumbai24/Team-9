7949b80e726ef7f81ec87c386632e23bea263f3b
Ashutoshbind15 <Ashutoshbind15@users.noreply.github.com>
2024-06-15 18:03:26 +0530

    code formatting rectify

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,76 +1,76 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcryptjs';
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema(
-	{
-		name: {
-			type: String,
-			required: true,
-		},
-		email: {
-			type: String,
-			required: true,
-			unique: true,
-		},
-		password: {
-			type: String,
-			required: true,
-		},
-		role: {
-			type: String,
-			enum: ['student', 'mentor', 'admin'],
-			required: true,
-		},
-		avatar: {
-			type: String,
-		},
-		district: {
-			type: String,
-		},
-		languages: [String],
-		courses: [
-			{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: 'Course',
-			},
-		],
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["student", "mentor", "admin"],
+      required: true,
+    },
+    avatar: {
+      type: String,
+    },
+    district: {
+      type: String,
+    },
+    languages: [String],
+    courses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
 
-		coursesTaught: [
-			{
-				type: mongoose.Schema.Types.ObjectId,
-				ref: 'Course',
-			},
-		],
-		mentorInformation: {
-			speciality: {
-				type: String,
-			},
-			qualification: {
-				type: String,
-			},
-			approval: {
-				type: Boolean,
-			},
-		},
-	},
-	{
-		timestamps: true,
-	}
+    coursesTaught: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+    mentorInformation: {
+      speciality: {
+        type: String,
+      },
+      qualification: {
+        type: String,
+      },
+      approval: {
+        type: Boolean,
+      },
+    },
+  },
+  {
+    timestamps: true,
+  }
 );
 
 userSchema.methods.matchPassword = async function (enteredPassword) {
-	return await bcrypt.compare(enteredPassword, this.password);
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
-userSchema.pre('save', async function (next) {
-	if (!this.isModified('password')) {
-		next();
-	}
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
 
-	const salt = await bcrypt.genSalt(10);
-	this.password = await bcrypt.hash(this.password, salt);
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model("User", userSchema);
 
 export default User;
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,25 +1,25 @@
-import express from 'express';
+import express from "express";
 import {
-	authUser,
-	registerUser,
-	logoutUser,
-	getUserProfile,
-	addMentorInfo,
-	approveMentorInfo,
-} from '../controllers/user.js';
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  addMentorInfo,
+  approveMentorInfo,
+} from "../controllers/user.js";
 import {
-	protect,
-	protectMentor,
-	protectAdmin,
-} from '../middleware/authMiddleware.js';
+  protect,
+  protectMentor,
+  protectAdmin,
+} from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.post('/', registerUser);
-router.post('/auth', authUser);
-router.post('/logout', logoutUser);
-router.route('/profile').get(protect, getUserProfile);
-router.route('/addMentorInfo').post(protect, protectMentor, addMentorInfo);
-router.route('/approveMentor').post(protect, protectAdmin, approveMentorInfo);
+router.post("/", registerUser);
+router.post("/auth", authUser);
+router.post("/logout", logoutUser);
+router.route("/profile").get(protect, getUserProfile);
+router.route("/addMentorInfo").post(protect, protectMentor, addMentorInfo);
+router.route("/approveMentor").post(protect, protectAdmin, approveMentorInfo);
 
 export default router;
