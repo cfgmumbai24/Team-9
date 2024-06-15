6856d72dae859dd38e949665deeb4572b7aba6ee
Ashutoshbind15 <Ashutoshbind15@users.noreply.github.com>
2024-06-16 00:39:22 +0530

    minor changes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,27 +1,8 @@
-<<<<<<< HEAD
-import Navbar from '../components/customUI/Navbar';
+import Navbar from "../components/customUI/Navbar";
 
 const Home = () => {
-	<Navbar />;
-	return <div>Home</div>;
+  <Navbar />;
+  return <div>Home</div>;
 };
-=======
-import React from 'react'
-import './Home.css'
->>>>>>> 3a879b8cfbecc2ca7b18f17a4aac8fbe282ac991
 
-const Home = () => {
-  return (
- 
-    <div className="hero-section">
-            <h1>Learn on your schedule</h1>
-            <p>Anywhere, anytime. Choose from thousands of expert-led courses now.</p>
-            <button className="hero-button">Get Started</button>
-    </div>
-    
-   
-   
-  )
-}
-
-export default Home
+export default Home;
--- frontend/src/pages/Temp.jsx
+++ frontend/src/pages/Temp.jsx
@@ -1,45 +1,14 @@
-import { useState } from 'react';
-import { useEdgeStore } from '../utils/edgestore';
+import { useState } from "react";
+import { useEdgeStore } from "../utils/edgestore";
 
 const Temp = () => {
-	const [file, setFile] = useState();
+  const [file, setFile] = useState();
 
-	const { edgestore } = useEdgeStore();
-	return (
-		<div>
-			<div>Temp</div>
+  const { edgestore } = useEdgeStore();
+  return (
+    <div>
+      <div>Temp</div>
 
-<<<<<<< HEAD
-			<input
-				type='file'
-				onChange={(e) => {
-					setFile(e.target.files?.[0]);
-				}}
-			/>
-			<button
-				onClick={async () => {
-					if (file) {
-						const res = await edgestore.publicFiles.upload({
-							file,
-							onProgressChange: (progress) => {
-								// you can use this to show a progress bar
-								console.log(progress);
-							},
-							input: {
-								category: 'temp',
-							},
-						});
-						// you can run some server action or api here
-						// to add the necessary data to your database
-						console.log(res);
-					}
-				}}
-			>
-				Upload
-			</button>
-		</div>
-	);
-=======
       <input
         type="file"
         onChange={(e) => {
@@ -70,9 +39,6 @@ const Temp = () => {
       </button>
     </div>
   );
->>>>>>> 3a879b8cfbecc2ca7b18f17a4aac8fbe282ac991
 };
 
-
-
-export default Temp
+export default Temp;
